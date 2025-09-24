9f35b55803b1a4b7b6352fed2e1e9abb8874c3d3
julianloaiza <loaizajulian1999@gmail.com>
2025-09-23 20:03:31 -0500

    chore: initial Spring Boot

---
+++ src/test/java/com/trade/demo/TradeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trade.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
