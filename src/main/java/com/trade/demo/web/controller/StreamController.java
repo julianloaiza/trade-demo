2adefd57c0985b0832060e6ef06c1849a45fbd8e
julianloaiza <loaizajulian1999@gmail.com>
2025-09-24 23:08:32 -0500

    refactor:  Applied direct Cors

--- src/main/java/com/trade/demo/web/controller/StreamController.java
+++ src/main/java/com/trade/demo/web/controller/StreamController.java
@@ -15,6 +15,7 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
 public class StreamController {
     
     private final KafkaTemplateImpl kafkaTemplate;
